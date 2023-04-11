23d1c6b0f4e5bd598ea9456375bcd320018c1882
MarcoBione <marcobione.work@gmail.com>
2023-04-11 16:49:03 +0200

    Add structure @ index + function for api @ script

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,21 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            
+    data() {
+        return {
+            //address of api
+            api: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            genNum: 0,
+            emailList:[],
         }
     },
-    methods:{
+    methods: {
+        getApis(){
+            axios.get(this.api).then((res) => {
+                console.log(this.genNum),
+                console.log(res.data.response);
+            })
+        }
 
     }
 }).mount('#app');
